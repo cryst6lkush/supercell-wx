3387706470101fdd1f353c2fa6df59a3af354df2
AdenKoperczak <awkoperczak@gmail.com>
2025-06-29 08:21:09 -0400

    fix live view of derived products overriding archived view

--- scwx-qt/source/scwx/qt/map/map_widget.cpp
+++ scwx-qt/source/scwx/qt/map/map_widget.cpp
@@ -2819,9 +2819,11 @@ void MapWidgetImpl::RadarProductManagerConnect()
                            }
                            else if (group == common::RadarProductGroup::Derived)
                            {
-                              // TODO
-                              widget_->SelectRadarProduct(
-                                 group, product, 0, latestTime, true);
+                              if (autoUpdateEnabled_)
+                              {
+                                 widget_->SelectRadarProduct(
+                                    group, product, 0, latestTime);
+                              }
                            }
                         }
                         catch (const std::exception& ex)
